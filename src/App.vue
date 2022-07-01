bc2fd2686438ba0f63225902206f3ff302f5b261
Tony Nguyen <tnguyen@activbrowser.com>
2022-07-01 10:55:40 +0200

    [fix] Add AppHeader to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
+  <app-header></app-header>
   <router-view></router-view>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue';
 export default {
   name: 'App',
+  components: {
+    AppHeader
+  }
 }
 </script>
 
