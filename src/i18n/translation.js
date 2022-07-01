9ed1c6efbf882f61df5a8d0a925af57dde9c1fa8
Tony Nguyen <tnguyen@activbrowser.com>
2022-07-01 16:55:56 +0200

    [fix] Add translation

--- src/i18n/translation.js
+++ src/i18n/translation.js
@@ -3,10 +3,14 @@ export default {
     addToCart: 'Ajouter au panier',
     cartTitle: 'Votre panier',
     proceedToPayment: 'Procéder au paiement',
+    searchPlaceholder: 'Chercher un produit ...',
+    total: 'total'
   },
   en: {
     addToCart: 'Add to cart',
     cartTitle: 'Your cart',
     proceedToPayment: 'Proceed to payment',
+    searchPlaceholder: 'Search a product ...',
+    total: 'total'
   }
 }
