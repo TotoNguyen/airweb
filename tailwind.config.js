374a3e6d003fa30631b263e8d4d00a8369718cb6
Tony Nguyen <tnguyen@activbrowser.com>
2022-07-01 02:14:20 +0200

    [fix] Add custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-app-bg': '#E5E5E5',
+        'gray-button-bg': '#4F4F4F',
+        'gray-total-bg': '#DADADA',
+        'gray-price-bg': '#EDEDED',
+        'gray-title': '#535353',
+        'gray-text': '#DEDEDE',
+        'black-text': '#292929',
+      },
+    },
   },
   plugins: [],
 }
