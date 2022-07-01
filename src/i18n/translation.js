c904d4d67daa6116c7630569394e4e2d08bfe73d
Tony Nguyen <tnguyen@activbrowser.com>
2022-07-01 12:44:27 +0200

    [feature] Add translation file

---
+++ src/i18n/translation.js
@@ -0,0 +1,12 @@
+export default {
+  fr: {
+    addToCart: 'Ajouter au panier',
+    cartTitle: 'Votre panier',
+    proceedToPayment: 'Procéder au paiement',
+  },
+  en: {
+    addToCart: 'Add to cart',
+    cartTitle: 'Your cart',
+    proceedToPayment: 'Proceed to payment',
+  }
+}
