0c7815896fa44c5f5a942317a151045d9dd5778c
Tony Nguyen <tnguyen@activbrowser.com>
2022-07-01 18:32:06 +0200

    [fix] Add modal translations

--- src/i18n/translation.js
+++ src/i18n/translation.js
@@ -4,13 +4,23 @@ export default {
     cartTitle: 'Votre panier',
     proceedToPayment: 'Procéder au paiement',
     searchPlaceholder: 'Chercher un produit ...',
-    total: 'total'
+    paymentSuccess: 'Paiement effectué !',
+    paymentSuccessText: 'Le paiement a été effectué !',
+    total: 'Total',
+    close: 'Fermer',
+    backToProductsList: 'Revenir à la sélection des produits',
+    productAddedToCart: 'Produit ajouté au panier',
   },
   en: {
     addToCart: 'Add to cart',
     cartTitle: 'Your cart',
     proceedToPayment: 'Proceed to payment',
     searchPlaceholder: 'Search a product ...',
-    total: 'total'
+    paymentSuccess: 'Payment done !',
+    paymentSuccessText: 'The payment has been successfully done !',
+    total: 'total',
+    close: 'Close',
+    backToProductsList: 'Back to products list',
+    productAddedToCart: 'Product added to the cart',
   }
 }
