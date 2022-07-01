3d50a552d6f69da498715bcb6da6555af0582849
Tony Nguyen <tnguyen@activbrowser.com>
2022-07-01 12:45:15 +0200

    [fix] Add i18n to App

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ import App from './App.vue';
 import store from './store';
 import router from './router';
 
+// Import translation
+import { createI18n } from 'vue-i18n';
+import translation from './i18n/translation.js';
+console.log(translation);
 // Import style
 import '@/assets/styles/main.css';
 import '@/assets/styles/tailwind.css';
 
+
+const i18n = createI18n({
+  locale: 'fr', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages: translation, // set locale messages
+})
+
 createApp(App)
   .use(store)
   .use(router)
+  .use(i18n)
   .mount('#app')
